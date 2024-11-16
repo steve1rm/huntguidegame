c61652b7cf87ceaa8bbbef55fb830195cd8861ed
Steve <steve@kitkat@gmail.com>
2024-11-16 20:02:08 +0700

    Adds the pictures to show

--- HuntGuide/src/main/java/me/androidbox/huntguide/presentation/screens/HuntGuideItemPage.kt
+++ HuntGuide/src/main/java/me/androidbox/huntguide/presentation/screens/HuntGuideItemPage.kt
@@ -1,12 +1,17 @@
 package me.androidbox.huntguide.presentation.screens
 
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun HuntGuideItemPage(
@@ -17,13 +22,31 @@ fun HuntGuideItemPage(
     @DrawableRes image: Int
 ) {
     Column(
-        modifier = modifier.fillMaxSize()
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween
     ) {
-        Text(text = title, color = Color.White)
+        Column {
+            Text(text = title, color = Color.White)
 
-        Text(text = subTitle, color = Color.White)
+            Text(text = subTitle, color = Color.White)
 
-        Text(text = body, color = Color.White)
+            Text(text = body, color = Color.White)
+        }
 
+        Image(
+            modifier = Modifier.aspectRatio(1f),
+            painter = painterResource(id = image),
+            contentDescription = null
+        )
     }
+}
+@Preview
+@Composable
+fun HuntGuideItemPagePreview() {
+    HuntGuideItemPage(
+        title = "Title",
+        subTitle = "Subtitle",
+        body = "Body",
+        image = android.R.drawable.ic_menu_report_image
+    )
 }
--- HuntGuide/src/main/java/me/androidbox/huntguide/presentation/screens/HuntGuideScreen.kt
+++ HuntGuide/src/main/java/me/androidbox/huntguide/presentation/screens/HuntGuideScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import me.androidbox.huntguide.R
 import me.androidbox.huntguide.presentation.models.HuntGuideModel
 import me.androidbox.huntguide.presentation.screens.components.HuntGuideItemPager
 
@@ -38,44 +39,44 @@ fun populateItems(): List<HuntGuideModel> {
             title = "01 MASTER YOURLOADOUT",
             subTitle = "Choose the Perfect Weapon Combo",
             body = "In your inventory you will be able to select your loadout by picking the two weapons you’ll rely on during the hunt. Each gun has unique strengths, so choose a combo that best matches your play style for maximum impact.",
-            res = 0
+            res = R.drawable.one
         ),
         HuntGuideModel(
             title = "02 elixir",
             subTitle = "your energy source and lifeline",
             body = "On Planet-X, everything you use is powered by elixir and you start each hunt with a full tank. This means your weapons, your scanner, your shield, anything with a power source relies on it.\n" +
                     "Since elixir is limited in each hunt, plan your usage carefully to get the most out of every action, giving yourself the best possible chance to win.",
-            res = 0
+            res = R.drawable.two
         ),
         HuntGuideModel(
             title = "03 elixir refill stations",
             subTitle = "top up your tank",
             body = "During a hunt, elixir refill stations are situated around the map. Use these to top up your elixir stores if you’re running low. However beware, other players will be looking to do the same thing so these are dangerous spots to visit!",
-            res = 0
+            res = R.drawable.three
         ),
         HuntGuideModel(
             title = "04 elixir vials",
             subTitle = "quick refill on the move",
             body = "You'll have the ability to take three elixir vials into each hunt if you have them in your inventory.  Vials can be bought in the store, stolen of other players or won in loot boxes. These allow you to quickly top up your elixir without having to go to a refill station. Perfect lifesavers for high-stakes moments like shootouts or prize scans. Use them strategically!",
-            res = 0
+            res = R.drawable.four
         ),
         HuntGuideModel(
             title = "05 activating your minimap",
             subTitle = "find an omni cell",
             body = "To unlock your mini-map you will have to Activate a Omni Cell situated on Planet-X. Omni cells shoot a distinct blue beam into the sky, which can be seen to try and locate them. Once your mini-map is activated you will be able to see key information such as other players carrying prizes, where chests have been found, how to find an extraction point and much more!\n",
-            res = 0
+            res = R.drawable.five
         ),
         HuntGuideModel(
             title = "06 scanning for chests",
             subTitle = "win prizes",
             body = "Your scanner allows you to find the hidden chests on Planet-X. Simply turn on your scanner and start running around to try and find one. If you find a chest you will be alerted as it pops out the ground. Then open the chest and pick up the prize.",
-            res = 0
+            res = R.drawable.six
         ),
         HuntGuideModel(
             title = "07 yellow and red chests",
             subTitle = "common and rare chests",
             body = "Your scanner allows you to find the hidden chests on Planet-X. Simply turn on your scanner and start running around to try and find one. If you find a chest you will be alerted as it pops out the ground. Then open the chest and pick up the prize.",
-            res = 0
+            res = R.drawable.seven
         ),
         HuntGuideModel(
             title = "08 extract a prize",
@@ -83,19 +84,19 @@ fun populateItems(): List<HuntGuideModel> {
             body = "If you happen to find a prize; you will need to extract it using an extraction rocket found on the mini-map to become an official winner. \n" +
                     "Extraction rockets are dangerous areas as you could come across campers trying to steal your prize.\n" +
                     "If you manage to successfully make it to the rocket, you will then need to input a 4 digit code which will appear on your screen for the extraction to be successful. \n",
-            res = 0
+            res = R.drawable.eight
         ),
         HuntGuideModel(
             title = "09 using your shield",
             subTitle = "master your defence",
             body = "Turning on your shield protects you from incoming fire whilst using elixir. This sheild however can be broken if it takes too much damage. If it does get broken, you’ll face a brief cooldown before being able to reactivate it, so time your defenses carefully!\n",
-            res = 0
+            res = R.drawable.nine
         ),
         HuntGuideModel(
             title = "10 using your drone",
             subTitle = "scout ahead tactically",
             body = "Use your drone situated under your mini-map to help you scout for nearby enemies or ensures areas of the map are clear, such as the extraction rocket. You can also use it to lock on to enemy players, which then gives you the ability to hunt them down or see if they’re holding a prize at anytime. Since it consumes elixir, use it strategically to maximize your advantage without depleting your resources.\n",
-            res = 0
+            res = R.drawable.ten
         ),
         HuntGuideModel(
             title = "11 stealing prizes",
@@ -104,7 +105,7 @@ fun populateItems(): List<HuntGuideModel> {
                     "\n" +
                     "Kill them and pick up their loot, to steal prizes. \n" +
                     "\n",
-            res = 0
+            res = R.drawable.eleven
         ),
         HuntGuideModel(
             title = "12 marked man indicators ",
@@ -114,7 +115,7 @@ fun populateItems(): List<HuntGuideModel> {
                     "If an enemy player is carrying a more valuable loot, an instant prize win, they will have a red pin above their head. \n" +
                     "\n" +
                     "Use these indicators to know decide whether you want to engage in a fight or not.",
-            res = 0
+            res = R.drawable.twelve
         )
     )
 }
